bfac4fbdd5f2adc4247030dcd5a2e33822a19981
AliWayne101 <alimalikwayne@gmail.com>
2023-06-07 20:39:44 +0500

    navbar placeholder and imgbb permission

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -10,11 +10,11 @@ const Navbar = () => {
 
     const { data: session } = useSession();
 
-  return (
-    <div className="sticky top-0 flex justify-between bg-secondary tsize-menu mb-5 z-10 blur-nav">
-        <div className="pl-5 pt-2 pb-2 flex">
-            <div className='mr-10'> <Link href={'/'}> <Logo /> </Link></div>
-            {/* <div className="p-2 menu-icon ml-2 pl-5 pr-5 rounded hidden sm:flex mt-4 mb-4">
+    return (
+        <div className="sticky top-0 flex justify-between bg-secondary tsize-menu mb-5 z-10 blur-nav">
+            <div className="pl-5 pt-2 pb-2 flex">
+                <div className='mr-10'> <Link href={'/'}> <Logo /> </Link></div>
+                {/* <div className="p-2 menu-icon ml-2 pl-5 pr-5 rounded hidden sm:flex mt-4 mb-4">
                 <SiSteam size={22} className='menu-icon'/>
             </div>
             <div className="p-2 menu-icon ml-2 pl-5 pr-5 rounded hidden sm:flex mt-4 mb-4">
@@ -23,31 +23,35 @@ const Navbar = () => {
             <div className="p-2 menu-icon ml-2 pl-5 pr-5 rounded hidden sm:flex mt-4 mb-4">
                 <SiUbisoft size={22} className='menu-icon'/>
             </div> */}
-        </div>
-        <div className="pr-5 pt-2 pb-2 flex mt-4">
-            {
-                session ? (
-                    session.user && (
-                        <>
-                        <Image
-                            src={session.user.image!}
-                            height={40}
-                            width={40}
-                            className='rounded-full'
-                            alt='profile'
-                            onClick={() => signOut()}
-                            title='Sign out'
-                        />
-                        <Link href={'/admin'}>{session.user.name}</Link>
-                        </> 
+            </div>
+            <div className="pr-5 pt-2 pb-2 flex mt-4">
+                {
+                    session ? (
+                        session.user && (
+                            <>
+                                <Image
+                                    src={
+                                        session.user.image
+                                            ? session.user.image
+                                            : "https://i.ibb.co/2NdhGP1/Profile-avatar-placeholder-large.png"
+                                    }
+                                    height={40}
+                                    width={40}
+                                    className='rounded-full'
+                                    alt='profile'
+                                    onClick={() => signOut()}
+                                    title='Sign out'
+                                />
+                                <Link href={'/admin'}>{session.user.name}</Link>
+                            </>
+                        )
+                    ) : (
+                        <span className='cursor-pointer flex' onClick={() => signIn('github')}><FiGithub size={32} />&nbsp;Login</span>
                     )
-                ) : (
-                    <span className='cursor-pointer flex' onClick={() => signIn('github')}><FiGithub size={32} />&nbsp;Login</span>
-                )
-            }
+                }
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Navbar
