23628092e8b0afc6dcc36edea22cf5b04a520192
AliWayne101 <alimalikwayne@gmail.com>
2023-06-07 21:33:10 +0500

    added entry styles

--- src/components/Entry.tsx
+++ src/components/Entry.tsx
@@ -5,21 +5,30 @@ import { IGame } from '@/schemas/GameSchema';
 import { RiComputerFill } from 'react-icons/ri';
 import { SiSteam, SiUbisoft, SiEpicgames } from 'react-icons/si';
 
-const Entry = ({entry, id}: {entry: IGame, id: number}) => {
+const Entry = ({ entry, id }: { entry: IGame, id: number }) => {
   return (
     <div className="w-full rounded-lg" id={`${id}`}>
-        <div className='relative pl-1 sm:pl-5 pr-1 sm:pr-5'>
-            <Image src={entry.image} alt={entry.name} fill className='entryImage rounded-lg shadow-2xl'/>
+      <div className='relative pl-1 sm:pl-5 pr-1 sm:pr-5'>
+        <Image src={entry.image} alt={entry.name} fill className='entryImage rounded-lg shadow-2xl' />
+      </div>
+      <div className="tsize-gtitle p-2 pt-5">{entry.name}</div>
+      <div className="flex justify-between p-2 tsize-small">
+        <div>{entry.genre}</div>
+        <div>
+          {entry.discounted === 0 ? (
+            entry.price
+          ) : (
+            <>
+              <span className="line-through mr-1">{entry.price}</span> {entry.discounted}
+            </>
+          )}
+          Rupees
         </div>
-        <div className="tsize-gtitle p-2 pt-5">{entry.name}</div>
-        <div className="flex justify-between p-2 tsize-small">
-            <div>{entry.genre}</div>
-            <div>{entry.price} Rupees</div>
-        </div>
-        <div className="flex justify-between p-2 mb-5">
-            <div>{entry.platform === "PC" && (<RiComputerFill size={20} className='mr-2' />)} { Icon(entry.host) }</div>
-            <div className='tsize-small flex'>Shop: &nbsp;<PaymentOptions size={20} /></div>
-        </div>
+      </div>
+      <div className="flex justify-between p-2 mb-5">
+        <div>{entry.platform === "PC" ? (<RiComputerFill size={20} className='mr-2' />) : entry.platform} {Icon(entry.host)}</div>
+        <div className='tsize-small flex'>Shop: &nbsp;<PaymentOptions size={20} /></div>
+      </div>
     </div>
   )
 }
@@ -39,7 +48,7 @@ const Icon = (_host: string) => {
     case "GOG":
       ICON = "GOG";
       break;
-    default: 
+    default:
       ICON = "NULL";
       break;
   }
